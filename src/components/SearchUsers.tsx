727bfba9e189b2082a462bb83f3d602e33e01fff
SuhrobTeshaev <125867321+SuhrobTeshaev@users.noreply.github.com>
2023-10-12 17:38:12 +0700

    add-css to form

--- src/components/SearchUsers.tsx
+++ src/components/SearchUsers.tsx
@@ -1,5 +1,5 @@
 import React,{FC, useState} from 'react'
-
+import './../style.css';
 interface SearchUsersProps {
     changeValueHandler:(field:string,event:React.ChangeEvent<HTMLInputElement>,setData:(str:string)=>void)=>void
 }
@@ -8,7 +8,7 @@ const SearchUsers:FC<SearchUsersProps>=({changeValueHandler})=>{
     const [userAge,setUserAge]=useState('');
     const [userName,setUserName]=useState('');
     return (
-        <div>
+        <div >
             <input 
             type="text"
             placeholder='name' 
